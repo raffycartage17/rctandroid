89a57a054b51ce0b0821de31f4419a8e1dece7e9
raffycartage17 <dashknow15@gmail.com>
2023-04-10 15:47:06 +0800

    Commit 0.2.9 AM April 10,2023 - 3:46PM

--- app/src/main/java/com/racartech/app/rctandroidlts/window1/Window1.java
+++ app/src/main/java/com/racartech/app/rctandroidlts/window1/Window1.java
@@ -1,5 +1,6 @@
 package com.racartech.app.rctandroidlts.window1;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -14,6 +15,7 @@ public class Window1 extends AppCompatActivity {
 
     ImageView image_view;
     Button file_picker_btn;
+    Button dialog_btn;
 
     private static final int FILE_PICKER_REQUEST_CODE = 1;
 
@@ -24,6 +26,7 @@ public class Window1 extends AppCompatActivity {
 
         image_view = findViewById(R.id.window1_imageview);
         file_picker_btn = findViewById(R.id.window1_filepicker_btn);
+        dialog_btn = findViewById(R.id.window1_open_dialog_button);
 
 
         file_picker_btn.setOnClickListener(new View.OnClickListener() {
@@ -34,6 +37,21 @@ public class Window1 extends AppCompatActivity {
                 startActivityForResult(intent, FILE_PICKER_REQUEST_CODE);
             }
         });
+        dialog_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dialog dialog = new Dialog(Window1.this);
+                dialog.setContentView(R.layout.standard_dialog_box);
+                dialog.setCancelable(true);
+                dialog.show();
+            }
+        });
+
+        /*
+        new_filedir_dialog = new Dialog(app_context);
+            new_filedir_dialog.setContentView(R.layout.newrename_dialog_box);
+            new_filedir_dialog.setCancelable(true);
+         */
 
     }
 
