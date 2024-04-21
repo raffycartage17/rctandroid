6efd8fd86a3972da86d7a273e73fc3ccceb59496
Laptop_Cartagena <dashknow15@gmail.com>
2024-04-21 16:55:19 +0800

    Commit 0.8.2 April 21, 2024 04:54PM

---
+++ rctandroid/src/main/java/com/racartech/library/rctandroid/file/util/RCTutilFilePicker.java
@@ -0,0 +1,26 @@
+package com.racartech.library.rctandroid.file.util;
+
+import android.app.Activity;
+import android.content.Context;
+import android.net.Uri;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RCTutilFilePicker{
+
+    int MODE_ALL = 0;
+    int MODE_IMAGE = 1;
+    int MODE_VIDEO = 2;
+    int MODE_AUDIO = 3;
+    int MODE_DOCUMENT = 4;
+
+    public RCTutilFilePicker(Activity activity, Context context, int mode){
+
+    }
+
+    public void dialogMode(){
+    }
+
+
+
+}
