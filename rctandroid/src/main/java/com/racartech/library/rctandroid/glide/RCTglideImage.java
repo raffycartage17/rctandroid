56aab58921697c6badc4be1e6b93589a9de6d38b
Laptop_Cartagena <dashknow15@gmail.com>
2024-05-09 20:23:40 +0800

    Commit 0.9.1 : May 9, 2024 - 08:23PM

--- rctandroid/src/main/java/com/racartech/library/rctandroid/glide/RCTglideImage.java
+++ rctandroid/src/main/java/com/racartech/library/rctandroid/glide/RCTglideImage.java
@@ -2,6 +2,7 @@ package com.racartech.library.rctandroid.glide;
 
 
 import android.content.Context;
+import android.net.Uri;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import com.bumptech.glide.Glide;
@@ -29,6 +30,49 @@ public class RCTglideImage {
                 .apply(requestOptions)
                 .into(image_button);
     }
+
+
+    public static void loadImageFromUrl(Context context, String imageUrl, ImageView imageView) {
+        RequestOptions requestOptions = new RequestOptions()
+                .diskCacheStrategy(DiskCacheStrategy.ALL);
+
+        Glide.with(context)
+                .load(imageUrl)
+                .apply(requestOptions)
+                .into(imageView);
+    }
+
+    public static void loadImageFromUrl(Context context, String imageUrl, ImageButton imageButton) {
+        RequestOptions requestOptions = new RequestOptions()
+                .diskCacheStrategy(DiskCacheStrategy.ALL);
+
+        Glide.with(context)
+                .load(imageUrl)
+                .apply(requestOptions)
+                .into(imageButton);
+    }
+
+
+    public static void loadImageFromUri(Context context, Uri uri, ImageView imageView) {
+        RequestOptions requestOptions = new RequestOptions()
+                .diskCacheStrategy(DiskCacheStrategy.ALL);
+
+        Glide.with(context)
+                .load(uri)
+                .apply(requestOptions)
+                .into(imageView);
+    }
+
+    public static void loadImageFromUri(Context context, Uri uri, ImageButton imageButton) {
+        RequestOptions requestOptions = new RequestOptions()
+                .diskCacheStrategy(DiskCacheStrategy.ALL);
+
+        Glide.with(context)
+                .load(uri)
+                .apply(requestOptions)
+                .into(imageButton);
+    }
+
 
 
 
