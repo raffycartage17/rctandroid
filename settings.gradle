dd5fa5bb3e7c1d0b233dc9177e1e4342e404cd22
raffycartage17 <dashknow15@gmail.com>
2024-07-05 19:42:14 +0800

    Commit 0.10.01 : July 05,2024 - 07:42PM

--- settings.gradle
+++ settings.gradle
@@ -4,7 +4,6 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
-
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
